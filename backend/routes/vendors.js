6ba11fe22f8068ff09330d4f0965848fbd5a8ed5
karthi206 <karthi26062006@gmail.com>
2025-07-27 22:10:17 +0530

    Fix vendors.json and connect API

--- backend/routes/vendors.js
+++ backend/routes/vendors.js
@@ -1,7 +1,10 @@
 fetch('https://street-food-vendor-7kvx.onrender.com/api/vendors')
 useEffect(() => {
-  fetch("https://street-food-vendor-7kvx.onrender.com/api/vendors")
-    .then(res => res.json())
-    .then(data => setVendors(data))
-    .catch(err => console.error("Error fetching vendors:", err));
+fetch("https://street-food-vendor-7kvx.onrender.com/api/vendors")
+.then(res => res.json())
+.then(data => {
+console.log("Vendors fetched:", data);
+setVendors(data);
+})
+.catch(err => console.error("Fetch error:", err));
 }, []);
