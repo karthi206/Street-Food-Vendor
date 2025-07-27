9731bfc6a5f530f214f0af9af6c4ade6105626aa
karthi206 <karthi26062006@gmail.com>
2025-07-27 22:03:34 +0530

    Update API fetch URL to live backend

--- backend/routes/vendors.js
+++ backend/routes/vendors.js
@@ -1,1 +1,7 @@
 fetch('https://street-food-vendor-7kvx.onrender.com/api/vendors')
+useEffect(() => {
+  fetch("https://street-food-vendor-7kvx.onrender.com/api/vendors")
+    .then(res => res.json())
+    .then(data => setVendors(data))
+    .catch(err => console.error("Error fetching vendors:", err));
+}, []);
